fa9062f0169f6db27572d937fa9689fc3afc5953
codewithshamim <shamimislamonline@gmail.com>
2022-06-05 16:03:14 +0600

    get experiences data

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
+require("dotenv").config();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d8hdu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const experienceCollection = client.db("air_cnc").collection("experiences");
+
+        app.get("/experience", async(req, res) => {
+            const experiences = await experienceCollection.find({}).toArray();
+            res.send(experiences);
+        })
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
     res.send("Server is running...")
